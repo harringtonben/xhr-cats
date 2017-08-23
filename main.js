38e893a9f29d41b9fe0fdf394435d06e03f34b8b
ben harrington <harringtonben@icloud.com>
2017-08-22 20:25:05 -0500

    created json file

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-var cats = [
-  {
-  name: "Fluffy", 
-  color: "Green", 
-  url: "https://metrouk2.files.wordpress.com/2015/01/ad_156114103-e1420794396375.jpg?w=748&h=505&crop=1"
-  },
-  {
-  name: "Spooky", 
-  color: "Blue", 
-  url: "https://i.pinimg.com/736x/05/c9/8a/05c98ad9c99a38282675c2f904c1468a--turkish-angora-cat-angora-cats.jpg"
-  },
-  {
-  name: "Idiot", 
-  color: "Purple", 
-  url: "https://i.amz.mshcdn.com/hRwKG_QIxWn6trBBUzHh924EoYo=/950x534/filters:quality(90)/2016%2F01%2F07%2F0a%2Fsmurfthekit.861f3.jpg"
-  },
-];
 
 function printCats() {
 	var catString = ``;
