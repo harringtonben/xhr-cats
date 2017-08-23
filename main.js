4abfbbaf26d7d7b98e57eddd64c5f52dd5654669
ben harrington <harringtonben@icloud.com>
2017-08-22 21:10:36 -0500

    finished xhr business

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 
-function printCats() {
+function printCats(cats) {
 	var catString = ``;
 	for (var i = 0; i < cats.length; i++){
 
-	
 	catString += `<div class="cat-card">`;
 	catString +=   `<h2>${cats[i].name}</h2>`;
 	catString +=   `<img class="cat" src="${cats[i].url}">`;
@@ -15,7 +14,37 @@ function printCats() {
 
 function showCrappyCats(strang) {
 	var catContainer = document.getElementById('kat-koral');
-	catContainer.innerHTML = strang; 
+	catContainer.innerHTML += strang; 
 }
 
-printCats(cats);
+// printCats(cats);
+
+function executeThisCodeAfterFileLoads() {
+	// console.log("this", this.responseText);
+	var data = JSON.parse(this.responseText);
+	printCats(data.cats);
+}
+
+function executeThisCodeAfterFileLoads2() {
+	// console.log("this", this.responseText);
+	var data = JSON.parse(this.responseText);
+	printCats(data.dogs);
+}
+
+function executeThisCodeIfFileErrors() {
+	console.log("shit broke");
+}
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", executeThisCodeAfterFileLoads);
+myRequest.addEventListener("error", executeThisCodeIfFileErrors);
+myRequest.open("GET", "cats.json");
+myRequest.send();
+
+var myRequest2 = new XMLHttpRequest();
+myRequest2.addEventListener("load", executeThisCodeAfterFileLoads2);
+myRequest2.addEventListener("error", executeThisCodeIfFileErrors);
+myRequest2.open("GET", "dogs.json");
+myRequest2.send();
+
+
